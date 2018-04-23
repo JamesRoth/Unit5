03857270ac81130aa4c5f3ce51c3a1d063c7af5e
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-23 08:18:37 -0400

    Updated from Brython Server: 4/23/2018 8:18:37 AM

--- fwordDemo.py
+++ fwordDemo.py
@@ -2,4 +2,7 @@
 #4/23/18
 #fwordDemo.py - printing words with an f in them
 
-words=input("Enter some words").split(" ")
+words=input("Enter some words: ").split(" ")
+
+for item in words:
+    print(item)
