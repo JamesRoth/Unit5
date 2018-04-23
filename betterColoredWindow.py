536d55dd710dddf506c8c5a1479c3862688075cc
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-23 08:45:55 -0400

    Updated from Brython Server: 4/23/2018 8:45:54 AM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -10,9 +10,9 @@ outline=LineStyle(1,Color(0x00ffff, 1))
 colors=[0x00ff00,0xff0000,0x0000ff,0x00ffff,0xff00ff,0xffffff]
 
 def mouseClick(event):
-    color=colors[0:randint(len(colors)-1)]
+    color=colors[randint(0,len(colors)-1)]
     
-    rectangle=RectangleAsset(1000, 2000, outline, (color,1))
+    rectangle=RectangleAsset(1000, 2000, outline, Color(color,1))
     Sprite(rectangle)
 
 App().listenMouseEvent("click", mouseClick)
