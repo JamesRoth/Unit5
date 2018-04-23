1fc7e4fa4801e7d82c830c2be018af8b8b43c72c
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-23 08:42:27 -0400

    Updated from Brython Server: 4/23/2018 8:42:26 AM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -1,1 +1,20 @@
+#James Roth
+#4/23/18
+#betterColoredWindow.py - function that changes a window's color
+
+from random import randint
+from ggame import *
+
+outline=LineStyle(1,Color(0x00ffff, 1))
+
+colors=[0x00ff00,0xff0000,0x0000ff,0x00ffff,0xff00ff,0xffffff]
+
+def mouseClick(event):
+    color=colors[0:randint(len(colors)-1)]
+    
+    rectangle=RectangleAsset(1000, 2000, outline, color)
+    Sprite(rectangle)
+
+App().listenMouseEvent("click", mouseClick)
+App().run()
 
