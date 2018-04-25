0f3be81ff890e5d15471f5efe406c3822f044748
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-25 08:20:08 -0400

    Updated from Brython Server: 4/25/2018 8:20:07 AM

--- displayDate.py
+++ displayDate.py
@@ -2,7 +2,7 @@
 #4/23/18
 #displayDate.py - current date with words
 
-from date import datetime
+from datetime import *
 
 days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 months=["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
