4a865be2ff3f8e1c90662fe2a966ca8874608b65
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-25 08:36:29 -0400

    Updated from Brython Server: 4/25/2018 8:36:28 AM

--- stat.py
+++ stat.py
@@ -1,1 +1,11 @@
+#James Roth
+#4/25/18
+#stat.py - median, mode, min, max, and mean of a list
+
+numbers=[]
+
+while True:
+    numbers.append(int(input("Enter a number to add to a list, type q when finished. "))
+    if App.listenKeyEvent() = "q"
+
 
