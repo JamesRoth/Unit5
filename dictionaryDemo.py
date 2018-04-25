5031fd6c0d5bf5c37fea28e0e55608da770849ec
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-25 08:07:18 -0400

    Update dictionaryDemo.py

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,11 @@
+#James Roth
+#4/25/18
+#dictionaryDemo.py - more list practice
 
+words=["computer", "mortify", "dog", "firetruck", "yes", "python", "cat"]
+
+words.sort()
+
+num=int(input("What number word do you want? "))
+num-=1
+print(words[num])
