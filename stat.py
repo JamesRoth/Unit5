289dca17ae1032d1aecd6f0ae07145857f2e9eac
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-26 08:50:23 -0400

    Updated from Brython Server: 4/26/2018 8:50:23 AM

--- stat.py
+++ stat.py
@@ -7,7 +7,7 @@ def median():
     if len(numbers)%2==0:
         print("Median: ", (numbers[(len(numbers)-1)/2]+numbers[len(numbers)/2])/2) 
     else:
-        print("Median: ", numbers[(len(numbers)+1)/2])
+        print("Median: ", numbers[(len(numbers)+1)/2-1])
 
 numbers=[]
 
