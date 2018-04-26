162ef45dfbb999b71b242bc2845a224ba0abe290
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-26 08:36:29 -0400

    Updated from Brython Server: 4/26/2018 8:36:29 AM

--- stat.py
+++ stat.py
@@ -6,6 +6,6 @@ numbers=[]
 
 while True:
     num=int(input("Enter a number to add to a list, type q when finished. "))
-    if num = "q":
+    if str(num) = "q":
         break
     numbers.append(num)
