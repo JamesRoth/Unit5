d302dd2e866613a29df0e2a6535946167f44bef8
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-26 08:24:22 -0400

    Updated from Brython Server: 4/26/2018 8:24:22 AM

--- vowelWords.py
+++ vowelWords.py
@@ -5,5 +5,5 @@
 words=input("Enter some words: ").split(" ")
 
 for item in words:
-    if item[0]=="a" or item[0]=="e" or item[0]=="i" or item[0]=="o" or item[0]=="u"
-    print(item)
+    if item[0]=="a" or item[0]=="e" or item[0]=="i" or item[0]=="o" or item[0]=="u":
+        print(item)
