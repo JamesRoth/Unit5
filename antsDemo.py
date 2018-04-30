e0a7db777ee93c8a4783e151be5f048404e2f4a7
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-30 08:06:33 -0400

    Updated from Brython Server: 4/30/2018 8:06:33 AM

--- antsDemo.py
+++ antsDemo.py
@@ -1,1 +1,11 @@
+#James Roth
+#4/30/18
+#antsDemo.py - graphics and lists
 
+from ggame import *
+
+ANTS=10
+
+if __name__ == "main":
+    red=Color(0xff0000,1)
+    ant=CircleAsset(20,LineStyle(1,red),red)
