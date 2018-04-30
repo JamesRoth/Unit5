ad9d4203b6b82709e57825eafe2acc086bc57aff
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-30 08:31:10 -0400

    Updated from Brython Server: 4/30/2018 8:31:09 AM

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,15 @@
+#James Roth
+#4/30/18
+#toDoList.py - makes a list of things to do
 
+tasks = []
+
+while False:
+    task=input("Enter a command (add, delete, print, quit): ")
+    if task == "quit":
+        break
+    elif task[0] == "a":
+        tasks.append(task[3:])
+    elif task[0] == "d":
+        task.remove(
+        
