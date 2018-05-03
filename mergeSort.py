76c6aec5c810dbec413792edea225ce95fcdcf69
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-03 08:32:04 -0400

    Updated from Brython Server: 5/3/2018 8:32:04 AM

--- mergeSort.py
+++ mergeSort.py
@@ -38,6 +38,8 @@ def merge(left, right):
         while len(right) =! 0:
             result.append(right[0])
             right.remove(right[0])
+        
+        return result
 
 if __name__ == '__main__':
 
