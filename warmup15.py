934c0990590ed5832f85ac67969b0b7e59162af7
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-04 08:15:07 -0400

    Updated from Brython Server: 5/4/2018 8:15:06 AM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,10 @@
+#James Roth
+#5/4/18
+#warmup15.py - doubling numbers in a list
 
+numbers=[8,3,2,6,4,7]
+
+def double(list):
+    doubled=[]
+    for i in range(0, list):
+        doubled.append(list[i]*2)
