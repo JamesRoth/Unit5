df8742c2891efc8ac57aa403496c1e4c7ff424f7
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-04 08:47:34 -0400

    Updated from Brython Server: 5/4/2018 8:47:34 AM

--- stat.py
+++ stat.py
@@ -2,14 +2,14 @@
 #4/25/18
 #stat.py - median, mode, min, max, and mean of a list
 
-def median():
+def median(): #finds the median
     numbers.sort()
     if len(numbers)%2==0:
-        print("Median: ", (numbers[(len(numbers)-1)/2]+numbers[len(numbers)/2])/2) 
+        print("Median: ", (numbers[(len(numbers)-1)/2]+numbers[len(numbers)/2])/2) #if the list has an even length
     else:
         print("Median: ", numbers[(len(numbers)+1)/2-1])
 
-def mode():
+def mode(): #finds the mode of the list
     storeLong=[0,0] #[the number, how many times it occurs]
     for item in numbers:
         num=numbers.count(item)
@@ -20,7 +20,7 @@ def mode():
 
 numbers=[]
 
-while True:
+while True: #loop for entering numbers into the list
     num=input("Enter a number to add to a list, type q when finished. ")
     if num == "q":
         break
