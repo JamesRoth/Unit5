1868d7d4826237d8aa69b8fa69fd630966e758b4
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-07 08:16:00 -0400

    Updated from Brython Server: 5/7/2018 8:15:58 AM

--- quiz5.py
+++ quiz5.py
@@ -8,9 +8,17 @@ def penultimate(list):
 def plusEquals(list, num):
     for i in range(0,len(list)):
         list[i]+=num
+    return(list)
 
 def smallest(list):
     small=list[0]
     for i in range(1,len(list)):
         if list[i]<small:
             small=list[i]
+    return(small)
+
+def decimalRange(num1, num2, num3):
+    list1=[]
+    if num1<num2:
+        for i in range(num1, num2, num3):
+            list1.append(i)
