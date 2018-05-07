15a3ecfc8aeca466b1470fc0f2a98bce16a32778
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-07 08:12:52 -0400

    Update quiz5.py

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,16 @@
+#James Roth
+#5/7/18
+#quiz5.py
 
+def penultimate(list):
+    return(list[-2])
+    
+def plusEquals(list, num):
+    for i in range(0,len(list)):
+        list[i]+=num
+
+def smallest(list):
+    small=list[0]
+    for i in range(1,len(list)):
+        if list[i]<small:
+            small=list[i]
